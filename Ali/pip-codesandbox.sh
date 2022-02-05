e07c11f1ea2553ca8d454bac5d7f9e22f6575883
MUHAMMAD ALI <malirashid1994@gmail.com>
2022-02-06 00:20:39 +0500

    removed npm package

--- Ali/pip-codesandbox.sh
+++ Ali/pip-codesandbox.sh
@@ -3,7 +3,5 @@ wget 'https://bootstrap.pypa.io/get-pip.py'
 python3 get-pip.py
 export PATH="$HOME/.local/bin:$PATH"
 pip install streamlit
-npm i express
-npm audit fix --force
-npm install -g --silent
+
 
