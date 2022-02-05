33b6dcfa42dd00433e1ffaf8b523e61bcbb42f61
MUHAMMAD ALI <malirashid1994@gmail.com>
2022-02-06 00:19:13 +0500

    removed test files

--- Ali/pip-codesandbox.sh
+++ Ali/pip-codesandbox.sh
@@ -3,10 +3,7 @@ wget 'https://bootstrap.pypa.io/get-pip.py'
 python3 get-pip.py
 export PATH="$HOME/.local/bin:$PATH"
 pip install streamlit
-git clone 'https://github.com/DigitalProductschool/AI-Makerspace.git'
-mv AI-Makerspace/PyCaret-Classification/* /sandbox
-cd /sandbox/
 npm i express
 npm audit fix --force
 npm install -g --silent
-streamlit run streamlit_app.py
+
