8d40d6ea8dbe4d9865e7af1b29c93cfad8ac0b07
MUHAMMAD ALI <malirashid1994@gmail.com>
2022-02-06 00:16:28 +0500

    Fixed sandbox path

--- Ali/pip-codesandbox.sh
+++ Ali/pip-codesandbox.sh
@@ -1,12 +1,12 @@
-﻿cd /
+cd /
 wget 'https://bootstrap.pypa.io/get-pip.py'
 python3 get-pip.py
 export PATH="$HOME/.local/bin:$PATH"
 pip install streamlit
 cd /
 git clone 'https://github.com/DigitalProductschool/AI-Makerspace.git'
-mv AI-Makerspace/PyCaret-Classification/* sandbox
-cd sandbox/
+mv AI-Makerspace/PyCaret-Classification/* /sandbox
+cd /sandbox/
 npm i express
 npm audit fix --force
 npm install -g --silent
